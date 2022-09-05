1f6898b456ceb5da10bb59f3e59c0ec34dab8492
Nick-117 <111483367+Nick-117@users.noreply.github.com>
2022-09-05 14:57:17 -0500

    Add files via upload

---
+++ FizzBuzz.py
@@ -0,0 +1,48 @@
+#Write your code below this row 👇
+
+
+
+for number in range(1, 101):
+
+    all_num = 0 + number
+
+
+
+
+
+    if number % 5 == 0 and number % 3 == 0:
+        
+        print("FizzBuzz")  
+
+           
+    elif number % 3 == 0:
+
+       all_num = print("Fizz")
+
+
+    elif number % 5 == 0:
+
+        all_num = print("Buzz")
+
+    
+    else:
+        print(all_num)    
+
+
+
+
+
+
+# for number in range(1, 101):
+
+#     for number in range(3, 101, 3):
+
+#         number = print("fizz")
+
+#     for number in range(5, 101, 5):
+
+#         number = print("Buzz")    
+
+
+    
+        
