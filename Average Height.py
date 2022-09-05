aeb5fe088352585106514cc25e3d6c8c831457ef
Nick-117 <111483367+Nick-117@users.noreply.github.com>
2022-09-05 15:00:50 -0500

    Add files via upload

---
+++ Average Height.py
@@ -0,0 +1,144 @@
+# 🚨 Don't change the code below 👇
+student_heights = input("Input a list of student heights ").split()
+for n in range(0, len(student_heights)):
+  student_heights[n] = int(student_heights[n])
+# 🚨 Don't change the code above 👆
+
+
+#Write your code below this row 👇
+
+
+
+total_height = 0
+
+for height in student_heights:
+
+    total_height += height 
+
+# print(total_height)
+
+
+divide_num = 0
+
+for num in student_heights:
+
+   divide_num += 1
+        
+# print(divide_num)
+
+
+Final_total = round(total_height / divide_num)
+
+print(Final_total)
+
+
+
+
+# total_divided = total_height / n 
+
+# print(total_divided)
+
+    
+
+
+# total = 0
+
+
+# for n in student_heights:
+
+#     # print(n)
+
+    
+
+#     final = total + n
+
+    
+# print(final)
+
+
+
+
+
+
+
+
+
+
+# y = 0 + student_heights[x]
+
+
+# print(y)
+
+
+
+
+
+
+
+     
+
+
+ 
+    
+
+
+
+    
+
+
+
+
+
+
+# print(total_amountplus)
+
+    # print(student_heights[x] + y)
+
+    # print(int(student_heights))
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# for z in range(0, len(student_heights)):
+
+#     student_heights[z] = int(student_heights[z])
+
+
+#     # print(student_heights[z] + 1)
+
+#     x = 0 + student_heights[n]
+
+#     print(range(0, )
+
+
+
+
+
+    # y = 0 + student_heights[z]
+
+    # a = x + y
+
+    # print(x + z)
+
+    # # print(student_heights[n] + y)
+
+
+
+
+
+    
+    
+
